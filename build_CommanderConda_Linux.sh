f351248c1926567266aa7c3dcf2b809a290afa22
Izaak Beekman <zbeekman@gmail.com>
2020-05-20 18:07:42 -0400

    Fetch/cache/verify miniforge on mac & test qemu

---
+++ build_CommanderConda_Linux.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+case "${ARCH}" in
+    x86_64)
+        DOCKER_ARCH=amd64
+        DOCKERIMAGE=condaforge/linux-anvil-comp7
+        ;;
+    ppc64le)
+        DOCKER_ARCH=ppc64le
+        DOCKERIMAGE=condaforge/linux-anvil-ppc64le
+        ;;
+    aarch64)
+        DOCKER_ARCH=arm64v8
+        DOCKERIMAGE=condaforge/linux-anvil-aarch64
+        ;;
+esac
+
+export DOCKER_ARCH
+export DOCKERIMAGE
+
+echo "============= Create build directory ============="
+mkdir -p build/ || true
+chmod 777 build/
+
+echo "============= Enable QEMU ============="
+docker run --rm --privileged multiarch/qemu-user-static:register --reset --credential yes
+
+# echo "============= Build the installer ============="
+# docker run --rm -ti -v "$(pwd)":/construct -e COMMANDERCONDA_VERSION -e COMMANDERCONDA_NAME $DOCKERIMAGE /construct/scripts/build.sh
+
+# echo "============= Test the installer ============="
+# for TEST_IMAGE_NAME in "ubuntu:19.10" "ubuntu:16.04" "ubuntu:18.04" "centos:7" "debian:buster"
+# do
+#   echo "============= Test installer on $TEST_IMAGE_NAME ============="
+#   docker run --rm -ti -v "$(pwd)":/construct -v "$(pwd)/build/qemu/qemu-${ARCH}-static:/usr/bin/qemu-${ARCH}-static" ${DOCKER_ARCH}/$TEST_IMAGE_NAME /construct/scripts/test.sh
+# done
---
+++ build_CommanderConda_macOS.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+echo "Installing Miniforge3."
+bash "build/miniforge/${MINIFORGE_FILE}" -b
+
+echo "Configuring conda."
+
+#shellcheck disable=SC1090
+source ~/miniforge3/bin/activate root
+
+CONSTRUCT_ROOT="$(pwd)"
+export CONSTRUCT_ROOT
+
+echo "============= Create build directory ============="
+mkdir -p build/ || true
+chmod 777 build/
+
+# echo "============= Build the installer ============="
+# bash scripts/build.sh
+
+# echo "============= Test the installer ============="
+# bash scripts/test.sh
